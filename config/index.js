c0c3597a2cf6d5096f364b74fd2f6691213ac9fe
lijunqi <lijq@zjitd.com>
2018-06-01 20:49:12 +0800

    2

---
+++ config/index.js
@@ -0,0 +1,59 @@
+// see http://vuejs-templates.github.io/webpack for documentation.
+var path = require('path')
+
+module.exports = {
+  build: {
+    env: require('./prod.env'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: './',
+    productionSourceMap: false,
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+    // Run the build command with an extra argument to
+    // View the bundle analyzer report after build finishes:
+    // `npm run build --report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: process.env.npm_config_report
+  },
+  dev: {
+    host: 'localhost', // can be overwritten by process.env.HOST
+    env: require('./dev.env'),
+    port: 8183,
+    autoOpenBrowser: true,
+    // assetsSubDirectory: 'static',
+    assetsSubDirectory: '',
+    assetsPublicPath: '/',
+    proxyTable: {
+      '/api': {
+        target: 'http://10.0.0.222:8981/ips/',
+        // target: 'http://localhost:3000/',
+        changeOrigin: true
+      },
+      '/user': {
+        // target: 'http://192.168.10.164:27005/ips/',
+        target: 'http://10.0.0.52:8086/ips/',
+        changeOrigin: true
+      },
+      '/myapi': {
+        // target: 'http://192.168.1.100:3000/',
+        target: 'http://localhost:3000/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/myapi': ''
+        }
+      }
+    },
+    // CSS Sourcemaps off by default because relative paths are "buggy"
+    // with this option, according to the CSS-Loader README
+    // (https://github.com/webpack/css-loader#sourcemaps)
+    // In our experience, they generally work as expected,
+    // just be aware of this issue when enabling this option.
+    cssSourceMap: false
+  }
+}
